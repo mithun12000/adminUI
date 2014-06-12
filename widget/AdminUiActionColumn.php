b0bc57c05e1f613075e99a8f639da22ee05cdfc4
Mithun Mandal <mithun12000@gmail.com>
2014-06-12 20:57:08 +0530

    update some base file for grid view support

---
+++ widget/AdminUiActionColumn.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\adminUi\widget;
+
+use Yii;
+use Closure;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+
+/**
+ * ActionColumn is a column for the [[GridView]] widget that displays buttons for viewing and manipulating the items.
+ *
+ * To add an ActionColumn to the gridview, add it to the [[GridView::columns|columns]] configuration as follows:
+ *
+ * ```php
+ * 'columns' => [
+ *     // ...
+ *     [
+ *         'class' => 'yii\grid\ActionColumn',
+ *         // you may configure additional properties here
+ *     ],
+ * ]
+ * ```
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AdminUiActionColumn extends ActionColumn
+{
+    /**
+     * Initializes the default button rendering callbacks
+     */
+    protected function initDefaultButtons()
+    {
+        if (!isset($this->buttons['view'])) {
+            $this->buttons['view'] = function ($url, $model) {
+                return Html::a('<span class="fa fa-eye"></span>', $url, [
+                    'title' => Yii::t('yii', 'View'),
+                    'data-pjax' => '0',
+                ]);
+            };
+        }
+        if (!isset($this->buttons['update'])) {
+            $this->buttons['update'] = function ($url, $model) {
+                return Html::a('<span class="fa fa-edit"></span>', $url, [
+                    'title' => Yii::t('yii', 'Update'),
+                    'data-pjax' => '0',
+                ]);
+            };
+        }
+        if (!isset($this->buttons['delete'])) {
+            $this->buttons['delete'] = function ($url, $model) {
+                return Html::a('<span class="fa fa-trash-o"></span>', $url, [
+                    'title' => Yii::t('yii', 'Delete'),
+                    'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                    'data-method' => 'post',
+                    'data-pjax' => '0',
+                ]);
+            };
+        }
+        
+        if (!isset($this->buttons['restore'])) {
+            $this->buttons['restore'] = function ($url, $model) {
+                return Html::a('<span class="fa fa-undo"></span>', $url, [
+                    'title' => Yii::t('yii', 'Restore'),
+                    'data-pjax' => '0',
+                ]);
+            };
+        }
+    }
+}
